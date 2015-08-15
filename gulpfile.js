62e8e4577aa1685e74ea5a3a98c2ef7759a23632
Shawn Rice <iamshawnrice@gmail.com>
2015-08-15 15:38:38 -0400

    7-1 pattern sass architecture installed; Bootstrap sass installed; Sass compiling via gulp;

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,35 @@
 var gulp = require('gulp');
 
+// gulp dependencies
 var autoprefixer = require('gulp-autoprefixer'),
-    bowerFiles = require('main-bower-files'),
     browserSync = require('browser-sync').create(),
-    buster = require('gulp-buster'),
     concat = require('gulp-concat'),
-    ngAnnotate = require('gulp-ng-annotate'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss');
 
-gulp.task('js', function() {
-  var files = bowerFiles();
-
-  files.push('./js/**/*.js');
-
-  gulp.src(files)
-    .pipe(sourcemaps.init())
-    .pipe(concat('scripts.js'))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./dist/'))
-    .pipe(browserSync.stream());
-});
-
-gulp.task('js-build', function() {
-  var files = bowerFiles();
-
-  files.push('./js/**/*.js');
-
-  gulp.src(files)
-    .pipe(concat('scripts.js'))
-    .pipe(ngAnnotate())
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'))
-});
+// Local vars
+var cssPath = './app/assets/css',
+    sassPath = './app/assets/sass/**/*.scss';
 
 gulp.task('sass', function () {
-  gulp.src('./sass/**/*.scss')
+  gulp.src(sassPath)
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./css'))
+    .pipe(gulp.dest(cssPath))
     .pipe(browserSync.stream());
-});
-
-gulp.task('sass-build', function () {
-  gulp.src('./sass/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(autoprefixer())
-    .pipe(uglifycss())
-    .pipe(buster())
-    .pipe(gulp.dest('./css'))
 });
 
 // Server + watching scss/js files
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
-    proxy: 'localhost:8888'
+    proxy: 'localhost:3000'
   });
 
-  gulp.watch('./sass/**/*.scss', ['sass']);
-  gulp.watch('./js/**/*.js', ['js']);
+  gulp.watch(sassPath, ['sass']);
 });
 
-gulp.task('build', ['sass-build', 'js-build']);
 gulp.task('default', ['serve']);
